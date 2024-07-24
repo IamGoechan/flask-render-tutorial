68d99d710a3bb26eefed1d2282d3028a8932ff11
IamGoechan <japandi.official@gmail.com>
2024-07-24 13:29:13 +0900

    Update my_flaskapp.py

--- my_flaskapp.py
+++ my_flaskapp.py
@@ -44,7 +44,7 @@ df['リードタイム'] = (df['申込受付日'] - df['チェックイン日'])
 server = Flask(__name__)
 
 # Dash アプリケーションの設定
-app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.CYBORG])
+app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # アプリケーションのレイアウト
 app.layout = dbc.Container([
